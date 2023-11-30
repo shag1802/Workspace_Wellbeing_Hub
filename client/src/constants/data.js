17a9c70f19c66a4e0cb0b67baf761e43176b0e3a
shag1802 <agrawalshaurya2002@gmail.com>
2023-11-30 23:16:52 +0530

    Changed Side Bar Topics

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -1,7 +1,7 @@
 export const categories = [
-    { id: 1, type: "Music" },
-    { id: 2, type: "Movies" },
-    { id: 3, type: "Sports" },
-    { id: 4, type: "Tech" },
-    { id: 5, type: "Fashion" }
+    { id: 1, type: "Announcements" },
+    { id: 2, type: "Support and Help" },
+    { id: 3, type: "Tech Trends and News" },
+    { id: 4, type: "General Discussion Forum" },
+    { id: 5, type: "Creativity Corner" }
 ];
